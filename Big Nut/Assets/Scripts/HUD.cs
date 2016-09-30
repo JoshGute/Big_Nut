2d0131888b7aa760c9bb6a64a9b8cca5fe9a60d2
GonefishingX <Canas090@yahoo.com>
2016-09-30 10:33:09 -0700

    Basic HUD and small mends, plus touched scene.

---
+++ Big Nut/Assets/Scripts/HUD.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour {
+
+    public PlayerController PlayerOne;
+    public PlayerController PlayerTwo;
+
+    public GameObject P1robs;
+    public GameObject P2robs;
+
+    public GameObject P1hp;
+    public GameObject P2hp;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+
+
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        P1hp.GetComponent<Text>().text = PlayerOne.bBody.fLifetime.ToString();
+        P2hp.GetComponent<Text>().text = PlayerTwo.bBody.fLifetime.ToString();
+    }
+
+    void UpdateBotsLeft()
+    {
+
+
+    }
+}
